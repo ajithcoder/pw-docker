4500fd31c613b70512d53cde55c3a76a817e1aac
ajithcoder <ajithelengical@gmail.com>
2025-02-13 10:05:14 +0100

    Adding cucumber adaptations to the framework

--- tests/loginTests.spec.ts
+++ tests/loginTests.spec.ts
@@ -1,13 +1,8 @@
-import {test, expect} from '@playwright/test'
-import { LoginPage } from '../page objects/loginPage.pom'
+import {test , expect} from '../page objects/fixtures.ts'
 
-test("Login with POM structure", async({page})=>{
+test("Login with POM structure", async({page, loginPage})=>{
   
-    const loginPage = new LoginPage(page);
-    await page.goto('https://binaryville.com/account');
-    await loginPage.emailLocator.fill('test@example.com');
-    await loginPage.passwordLocator.fill('pass123');
-    await loginPage.signInButtonLocator.click();
+    await loginPage.login('test@example.com','pass123')
     const url = page.url();
     expect (url).toContain('pass123')
     
